069908e31ddcface3656fe3d0d850abcfc80dc20
Hristo Kosev <smanuel@healee.com>
2022-06-08 00:28:58 +0300

    Type definitions - protocolOperations

--- @ts-types/global.d.ts
+++ @ts-types/global.d.ts
@@ -1,1 +1,22 @@
-declare var showConfirm: (x: string, y: () => void, z?: () => void) => void;
+declare const heal: {
+	client: {
+		protocolOperations: {
+			checkAnswerMatch(forms: any[], keys: any): boolean,
+			getAnswer(forms: any[], answerId: string): string,
+			getAnswerData(forms: any[], answerId: string): any,
+			getFormData(protocol: any, id: string, findLast?: boolean): any,
+			getNotification(protocol: any, findLast?: boolean): any,
+			createNotification(group: string, componentId: string, text: string, data?: any): any,
+			setRecurrence(notification: any, startDate: Date, period: string, value: number, skipEvery?: number): void,
+			consts: {
+				MONTH_PERIOD: string,
+				DAY_PERIOD: string,
+				YEAR_PERIOD: string,
+				HOUR_PERIOD: string,
+				MINUTE_PERIOD: string
+			}
+		}
+	}
+};
+
+declare const showConfirm: (message: string, confirmCallback: () => void, cancelCallback?: () => void) => void;
