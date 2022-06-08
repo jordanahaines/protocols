be77071babc6d5bf8db6987c1678f5da93513350
Hristo Kosev <smanuel@healee.com>
2022-06-08 14:23:33 +0300

    Protocol tool improvements - notify mode, exclude .git folder from sync

--- protocol.sh
+++ protocol.sh
@@ -5,9 +5,15 @@ protocol=$2
 
 if [ "$mode" = "fetch" ]; then
 	aws s3 sync --profile $protocol --delete s3://protocol-$protocol ./development/$protocol
+elif [ "$mode" = "notify" ]; then
+	curl -w "\n" -H "Authorization: Basic $HEALEE_STAGE_AUTH" -X POST $HEALEE_STAGE_URL/api/v1/notifyProtocol/$protocol
 elif [ "$mode" = "build" ]; then
-	aws s3 sync --profile $protocol --delete ./development/$protocol s3://protocol-$protocol
-	curl -w "\n" -H "Authorization: Basic $HEALEE_STAGE_AUTH" -X POST $HEALEE_STAGE_URL/api/v1/buildProtocol/$protocol
+	if [ -f "./development/$protocol/info.json" ]; then
+		aws s3 sync --profile $protocol --delete --exclude ".git/*" ./development/$protocol s3://protocol-$protocol
+		curl -w "\n" -H "Authorization: Basic $HEALEE_STAGE_AUTH" -X POST $HEALEE_STAGE_URL/api/v1/buildProtocol/$protocol
+	else
+		echo "Protocol doesn't exist. Please, call fetch to retrieve the initial version of the protocol."
+	fi
 else
-	echo "Try running: ./protocols.sh fetch|build [protocol name]"
+	echo "Try running: ./protocols.sh fetch|build|notify [protocol name]"
 fi
