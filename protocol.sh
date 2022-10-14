d13941d3f16470006c25373671019c5f93105856
Emil Atanasov <heitara@gmail.com>
2022-10-15 00:25:37 +0300

    extend protocols.sh to use rules file from the protocol folder

--- protocol.sh
+++ protocol.sh
@@ -9,7 +9,19 @@ elif [ "$mode" = "notify" ]; then
 	curl -w "\n" -H "Authorization: Basic $HEALEE_STAGE_AUTH" -X POST $HEALEE_STAGE_URL/api/v1/notifyProtocol/$protocol
 elif [ "$mode" = "build" ]; then
 	if [ -f "./development/$protocol/info.json" ]; then
-		aws s3 sync --profile $protocol --delete --exclude ".git/*" --exclude "node_modules/*" --exclude "gallery-widget/*" --exclude "runner/*" --exclude ".idea/*"  --exclude ".DS_Store" ./development/$protocol s3://protocol-$protocol
+		# default exclude
+		exclude="--exclude \".git/*\" --exclude \"node_modules/*\" --exclude \"gallery-widget/*\" --exclude \"runner/*\" --exclude \".idea/*\"  --exclude \".DS_Store\""
+		# if exclude.rules exists this will override the eclude rules
+		if [ -f "./development/$protocol/exclude.rules" ]; then
+			# update exclude rules
+			input="./development/$protocol/exclude.rules"
+			exclude=""
+			while IFS= read -r line
+			do
+				exclude=$exclude" --exclude \"$line\""
+			done < "$input"
+		fi
+		aws s3 sync --profile $protocol --delete $exclude ./development/$protocol s3://protocol-$protocol
 		curl -w "\n" -H "Authorization: Basic $HEALEE_STAGE_AUTH" -X POST $HEALEE_STAGE_URL/api/v1/buildProtocol/$protocol
 	else
 		echo "Protocol doesn't exist. Please, call fetch to retrieve the initial version of the protocol."
