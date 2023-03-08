5c5c18112a99819f23a28e74fd8188bbfbbfefde
Erjan Gavalji <erjan.gavalji@gmail.com>
2023-03-08 15:05:26 +0200

    Ensure same-sized files will be synced

--- protocol.sh
+++ protocol.sh
@@ -4,12 +4,12 @@ mode=$1
 protocol=$2
 
 if [ "$mode" = "fetch" ]; then
-	aws s3 sync --profile $protocol --delete s3://protocol-$protocol ./development/$protocol
+	aws s3 sync --exact-timestamps --profile $protocol --delete s3://protocol-$protocol ./development/$protocol
 elif [ "$mode" = "notify" ]; then
 	curl -w "\n" -H "Authorization: Basic $HEALEE_STAGE_AUTH" -X POST $HEALEE_STAGE_URL/api/v1/notifyProtocol/$protocol
 elif [ "$mode" = "build" ]; then
 	if [ -f "./development/$protocol/info.json" ]; then
-		aws s3 sync --profile $protocol --delete --exclude ".git/*" --exclude "node_modules/*" --exclude "package-lock.json" ./development/$protocol s3://protocol-$protocol
+		aws s3 sync --exact-timestamps --profile $protocol --delete --exclude ".git/*" --exclude "node_modules/*" --exclude "package-lock.json" ./development/$protocol s3://protocol-$protocol
 		curl -w "\n" -H "Authorization: Basic $HEALEE_STAGE_AUTH" -X POST $HEALEE_STAGE_URL/api/v1/buildProtocol/$protocol
 	else
 		echo "Protocol doesn't exist. Please, call fetch to retrieve the initial version of the protocol."
